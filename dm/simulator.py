577e1ed8d411de9aec0ea08c5d1adba3f9444883
rugwing <aac.wong@gmail.com>
2021-11-28 20:10:13 -0800

    more useful output

--- dm/simulator.py
+++ dm/simulator.py
@@ -15,12 +15,13 @@ class Simulator:
         if action_type==ActionType.ACC_CHECKS:
             results,cost = self.game.attempt_accessible_check(action)
             if game.is_location(results):
-                print(f'!stopped at {results}!')
+                print(f'!stopped at {results} at level {self.game.player.level}!')
             elif results != None:
                 for result in results:
                     print(f'obtained {result}')
+                    self.agent.observe(action,result)
             else: 
-                print('!!failed to get check!!')
+                print(f'!!failed to get check at level {self.game.player.level}!!')
         elif action_type==ActionType.CHECKS_LOCS:
             results,cost = self.game.attempt_action(action)
             if game.is_location(results):
@@ -28,14 +29,15 @@ class Simulator:
             elif results != None:
                 for result in results:
                     print(f'obtained {result}')
+                    self.agent.observe(action,result)
             else: 
-                print('!!failed to get check!!')
+                print(f'!!failed to get check at level {self.game.player.level}!!')
     
     def simulate(self):
         while not self.game.is_finished():
             self.simulate_step()
-            print(f'game time: {self.game.time:.1f}')
-        print(f' completed game in {self.game.time:.2f}')
+            print(f'game time: {self.game.time/60:.2f} min')
+        print(' completed game in '+convert_sec_to_str(self.game.time))
 
     def simulate_plot(self):
         while not self.game.is_finished():
@@ -43,3 +45,11 @@ class Simulator:
             self.game.plot()
             print(f'game time: {self.game.time:.1f}')
         print(f' completed game in {self.game.time:.2f}')
+
+def convert_sec_to_str(sec):
+    mins = sec//60
+    hrs = int(mins//60)
+    mins = int(mins%60)
+    secs = sec-hrs*3600-mins*60
+    return f'{hrs} h {mins} m {secs} s'
+
